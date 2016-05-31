244145d6eb06e5426020bf5fe59b2c56df364023
kewang <cpckewang@gmail.com>
2016-05-31 23:01:53 +0800

    Add quickstart

---
+++ src/main/java/com/mitake/sms/App.java
@@ -0,0 +1,13 @@
+package com.mitake.sms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
