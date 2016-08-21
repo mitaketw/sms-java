cfb295b1e0ed8e558969928f67d7424cfb020679
kewang <cpckewang@gmail.com>
2016-08-21 11:55:20 +0800

    Add test limitation

--- src/test/java/tw/com/mitake/sms/SmsQueryTest.java
+++ src/test/java/tw/com/mitake/sms/SmsQueryTest.java
@@ -2,12 +2,16 @@ package tw.com.mitake.sms;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tw.com.mitake.sms.result.MitakeSmsQueryAccountPointResult;
 import tw.com.mitake.sms.result.MitakeSmsQueryMessageStatusResult;
 
 import static org.junit.Assert.assertNotNull;
 
 public class SmsQueryTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SmsQueryTest.class);
+
     @Before
     public void setUp() {
         MitakeSms.init(System.getenv("MITAKE_SMS_USERNAME"), System.getenv("MITAKE_SMS_PASSWORD"));
@@ -24,6 +28,13 @@ public class SmsQueryTest {
 
     @Test
     public void testGetMessageStatus() {
+        // because server-side limits 1 req/s
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            LOG.error(e.getMessage());
+        }
+
         MitakeSmsQueryMessageStatusResult result = MitakeSms.queryMessageStatus("messageid");
 
         System.out.println("result: " + result.toString());
